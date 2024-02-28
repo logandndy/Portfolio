e3ca3e1d30ea6ea47798fe3a46d8bc5b2fc2e15a
logdnd <dondaylogan2@gmail.com>
2024-02-28 15:27:41 +0100

    1er save

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import Knowledges from "./pages/Knowledges";
+import NotFound from "./pages/NotFound";
+import Portfolio from "./pages/Portfolio";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/Knowledges" element={<Knowledges />} />
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="/Portfolio" element={<Portfolio />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/pages/Knowledges.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+const Knowledges = () => {
+  return (
+    <div>
+      <Navigation />
+    </div>
+  );
+};
+
+export default Knowledges;
