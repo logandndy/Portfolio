45362285f59ad3f127eb591a539a60d97dd00547
logdnd <dondaylogan2@gmail.com>
2024-04-26 16:20:00 +0200

    26/04

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,6 +9,8 @@ const Home = () => {
         <div className="content">
           <h1>Donday Logan</h1>
           <h2>Développeur web</h2>
+          <h2>Webdesign</h2>
+          <h2>Design UI/UX</h2>
           <div className="pdf">
             <a
               href="/Mon objectif est de me professionnaliser dans le métier de Designer UXUI.pdf"
